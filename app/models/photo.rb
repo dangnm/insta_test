906a86a08a3fc8907019b655d2f4692dad7f3566
Dang Nguyen Minh <Dang@Dangs-MacBook-Air.local>
2015-08-28 23:38:52 +0700

    format code

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -36,18 +36,18 @@ class Photo < ActiveRecord::Base
   end
 
   def self.search_photo_by_hash_tags(tags)
-    @photos = []
-    @hash_tags = SimpleHashtag::Hashtag.where(name: tags)
-    @hash_tags.each do |hash_tag|
+    photos = []
+    hash_tags = SimpleHashtag::Hashtag.where(name: tags)
+    hash_tags.each do |hash_tag|
       hash_tag.hashtaggables.each do |tag_object|
         if tag_object.class == Photo
-          @photos.push(tag_object) unless @photos.include?(tag_object)
+          photos.push(tag_object) unless photos.include?(tag_object)
         elsif tag_object.class == Comment
-          @photos.push(tag_object.photo) unless @photos.include?(tag_object.photo)
+          photos.push(tag_object.photo) unless photos.include?(tag_object.photo)
         end
       end
     end
-    @photos
+    photos
   end
 
 end
