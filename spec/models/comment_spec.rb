de11a823a1bb5e06b50314d1c9a147b0a7d47ea4
Dang Nguyen Minh <Dang@Dangs-MacBook-Air.local>
2015-08-29 14:45:49 +0700

    update comment & photo test cases

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -13,5 +13,41 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#photo_comments' do 
+    before do
+      stub_const("Comment::COMMENTS_PER_PAGE", 4)
+    end
+    context "The photo has more than 4 comments" do
+      let!(:user_1) { create(:user, email: 'test2@test.com')}
+      let!(:photo_1) { create(:photo, user: user_1, caption: 'Good')}
+      let!(:comments_1) do 
+        7.times.collect {|i| create(:comment, user: user_1, 
+                          photo: photo_1,
+                          message: "test_#{i}") }
+      end
+
+      before do
+        @comments_page_1 = Comment.photo_comments(photo_1, page: 1)
+        @comments_page_2 = Comment.photo_comments(photo_1, page: 2)
+      end
+
+      it "returns 4 last comments in normal order of timeline
+          on the first page" do
+        expect(@comments_page_1.to_a.size).to eq(4)
+        expect(@comments_page_1.to_a[0]).to eq(comments_1[3])
+        expect(@comments_page_1.to_a[1]).to eq(comments_1[4])
+        expect(@comments_page_1.to_a[2]).to eq(comments_1[5])
+        expect(@comments_page_1.to_a[3]).to eq(comments_1[6])
+      end
+
+      it "returns more comments previous to 4 last 
+          comments in normal order of timeline on the next page" do
+        expect(@comments_page_2.to_a.size).to eq(3)
+        expect(@comments_page_2.to_a[0]).to eq(comments_1[0])
+        expect(@comments_page_2.to_a[1]).to eq(comments_1[1])
+        expect(@comments_page_2.to_a[2]).to eq(comments_1[2])
+      end
+    end
+
+  end
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -17,15 +17,79 @@ RSpec.describe Photo, type: :model do
     before do
       stub_const("Photo::PHOTOS_PER_PAGE", 9)
     end
-    let!(:user1) { create(:user, :email => 'test@test.com')}
-    let!(:user2) { create(:user, :email => 'test2@test.com')}
-    let!(:photos1) { create_list(:photo, 10, :user => user1)}
-    let!(:photos2) { create_list(:photo, 5, :user => user2)}
-    it "returns user photos" do
-      photos_1_page_1 = Photo.my_photos(user1, {:page => 1})
-      photos_1_page_2 = Photo.my_photos(user1, {:page => 2})
-      expect(photos_1_page_1.to_a.size).to eq(9)
-      expect(photos_1_page_2.to_a.size).to eq(1)
+    let!(:user1) { create(:user, email: 'test@test.com')}
+    let!(:user2) { create(:user, email: 'test2@test.com')}
+    let!(:photos1) { create_list(:photo, 10, user: user1)}
+    let!(:photos2) { create_list(:photo, 5, user: user2)}
+    before do  
+      @photos_1_page_1 = Photo.my_photos(user1, {page: 1})
+      @photos_1_page_2 = Photo.my_photos(user1, {page: 2})
+    end
+    it "returns 9 photos on the first page" do
+      expect(@photos_1_page_1.to_a.size).to eq(9)
     end 
+
+    it "returns 1 photo on the second page" do
+      expect(@photos_1_page_2.to_a.size).to eq(1)
+    end 
+
+    it "does not contain photos of other users" do
+      expect(@photos_1_page_1).not_to include(*photos2)
+      expect(@photos_1_page_2).not_to include(*photos2)
+    end
   end
+
+  describe '#my_photos_feed' do
+    before do
+      stub_const("Photo::PHOTOS_FEED_PER_PAGE", 4)
+    end
+    context "query photo feed of user 1 which follows user 2" do
+      let!(:user1) { create(:user, email: 'test@test.com')}
+      let!(:user2) { create(:user, email: 'test2@test.com')}
+      let!(:user3) { create(:user, email: 'test3@test.com')}
+      let!(:photos1) { create_list(:photo, 4, user: user1)}
+      let!(:photos2) { create_list(:photo, 3, user: user2)}
+      let!(:photos3) { create_list(:photo, 3, user: user3)}
+      before do 
+        user1.follow!(user2)
+        @photos_1_page_1 = Photo.my_photos_feed(user1, {page: 1})
+        @photos_1_page_2 = Photo.my_photos_feed(user1, {page: 2})
+      end
+      it "returns 4 photos of his photos or his follower photos
+          on the first page" do
+        expect(@photos_1_page_1.to_a.size).to eq(4)
+      end
+
+      it "returns 3 photos of his photos or his follower photos
+          on the second page" do
+        expect(@photos_1_page_2.to_a.size).to eq(3)
+      end
+
+      it "does not contain photos of unfollowed users" do 
+        expect(@photos_1_page_1).not_to include(*photos3)
+        expect(@photos_1_page_2).not_to include(*photos3)
+      end
+    end
+  end
+
+  describe '#search_photo_by_hash_tags' do 
+    context "Photos which have been tagged on caption & comments" do
+      let!(:user_1) { create(:user, email: 'test@test.com')}
+      let!(:photo_1) { create(:photo, user: user_1, caption: 'Good #test')}
+      let!(:comment_1_1) { create(:comment, user: user_1, 
+                               photo: photo_1,
+                               message: 'Good #test')}
+      let!(:photo_2) { create(:photo, user: user_1, caption: 'Good #test')}
+      let!(:comment_2_1) { create(:comment, user: user_1, 
+                               photo: photo_2,
+                               message: 'Good #test')}
+      it "returns unique photos" do
+        photos = Photo.search_photo_by_hash_tags('test')
+        expect(photos.to_a.size).to eq(2)
+        expect(photos).to include(photo_1)
+        expect(photos).to include(photo_2)
+      end
+    end
+end
+
 end
